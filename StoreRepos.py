b157cf49c5237cab9a9ba7061cf398317a81f32d
Marcio Junior <marciotojr@gmail.com>
2018-07-03 08:52:14 -0300

    Store repos basic info

--- StoreRepos.py
+++ StoreRepos.py
@@ -3,17 +3,31 @@ import mysql.connector
 
 valid = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
 
+def get_mysql_cursor():
+    db = mysql.connector.connection.MySQLConnection(user='root', password='',
+                                                     host='localhost',
+                                                     database='npm-github-fetcher')
+    return db, db.cursor()
 
 def get_repo(repo):
     repo = repo[:repo.rfind(',')]
     try:
         data = json.loads(repo)
-        return [data['id'], data['value']['_nodeVersion']]
+        url = str(data['value']['repository']['url'])
+        if 'github.com' in url:
+            if '.git' in url:
+                url = url.replace('.git', '')
+            if url[-2:-1] == '/':
+                url = url.replace('/', '')
+            lpos = url.rfind('/', 0, url.rfind('/'))
+            url = url[lpos+1:]
+            arr = [data['id'], data['value']['_nodeVersion'], url]
+            return arr
     except:
         return None
 
 
-def read_repos(file_path='npm_repos.json'):
+def read_repos_bak(file_path='npm_repos.json'):
     with open(file_path) as file:
         i = 1
         db = mysql.connector.connection.MySQLConnection(user='root', password='',
@@ -24,6 +38,7 @@ def read_repos(file_path='npm_repos.json'):
                 repo = get_repo(line)
                 #print(repo)
                 if repo is not None:
+                #if False:
                     if i % 30000 == 0:
                         db.commit()
 
@@ -38,8 +53,8 @@ def read_repos(file_path='npm_repos.json'):
                     # prepare a cursor object using cursor() method
                     cursor = db.cursor()
 
-                    q = "INSERT INTO npmprojects (id, name, version) VALUES (%s,%s,%s)"
-                    data = [i, repo[0], repo[1]]
+                    q = "INSERT INTO npmprojects (id, name, version, repo) VALUES (%s,%s,%s,%s)"
+                    data = [i, repo[0], repo[1], repo[2]]
 
                     # execute SQL query using execute() method.
                     cursor.execute(q, data)
@@ -47,13 +62,13 @@ def read_repos(file_path='npm_repos.json'):
                     print('Id: \t ' + str(i) + '\t\t\t' + 'Name:\t' + repo[0] + '\t\t\t' + repo[1])
 
                     i += 1
-
             finally:
                 pass
         db.commit()
 
         # disconnect from server
         db.close()
+        print(i)
 
 
 def version_to_float(version_str):
@@ -76,5 +91,79 @@ def version_to_float(version_str):
         return 0
 
 
+def read_repos(file_path='npm_repos.json'):
+    with open(file_path) as file:
+        i = 1
+        db = mysql.connector.connection.MySQLConnection(user='root', password='',
+                              host='localhost',
+                              database='npm-github-fetcher')
+        for line in file:
+            try:
+                repo = get_repo(line)
+                #print(repo)
+                if repo is not None:
+                #if False:
+                    if i % 30000 == 0:
+                        db.commit()
 
-read_repos()
+                        # disconnect from server
+                        db.close()
+
+                        # Open database connection
+                        db = mysql.connector.connection.MySQLConnection(user='root', password='',
+                                  host='localhost',
+                                  database='npm-github-fetcher')
+
+                    # prepare a cursor object using cursor() method
+                    cursor = db.cursor()
+
+                    repository = repo[2]
+
+                    q = "INSERT INTO npmprojects (id, name, version, repo) VALUES (%s,%s,%s,%s)"
+                    data = [i, repo[0], repo[1], repo[2]]
+
+                    # execute SQL query using execute() method.
+                    cursor.execute(q, data)
+
+                    print('Id: \t ' + str(i) + '\t\t\t' + 'Name:\t' + repo[0] + '\t\t\t' + repo[1])
+
+                    i += 1
+            finally:
+                pass
+        db.commit()
+
+        # disconnect from server
+        db.close()
+        print(i)
+
+
+def prioritize():
+    dbm, cursor = get_mysql_cursor()
+
+    query = 'SELECT id FROM npmprojects ORDER BY version DESC'
+    cursor.execute(query)
+
+    i=0
+
+    print("\n\n---------------------\n Prioritizing \n ")
+
+    db, loop_cursor = get_mysql_cursor()
+
+    for queried_data in cursor:
+
+        print(i)
+        data = [i, queried_data[0]]
+        query = 'UPDATE npmprojects SET priorization_order=%s WHERE id=%s'
+        loop_cursor.execute(query, data)
+
+        if i % 3000 == 0:
+            db.commit()
+
+        i += 1
+    db.commit()
+    db.close()
+    dbm.close()
+
+
+#read_repos()
+prioritize()
