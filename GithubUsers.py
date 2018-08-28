55f46b90e43f6e8373437b3dca288db05ffb0ca7
Marcio Junior <marciotojr@gmail.com>
2018-08-28 11:05:24 -0300

    Finished the fetch_repo_info function and its dependencies Added a Error.py file for specific errors

--- GithubUsers.py
+++ GithubUsers.py
@@ -4,34 +4,31 @@ import json
 from JSON_utils import trim_json, JSONEncoder, parse
 from User_type_manager import get_type_id
 
-user_keys_to_keep = ['id', 'login', 'name', 'company', 'blog', 'email', 'hireable', 'bio', 'followers', 'following',
-                     'type', 'contributions']
-non_persitant_keys = ['contributions']
+persistent_keys = ['id', 'login', 'name', 'company', 'blog', 'email', 'hireable', 'bio', 'followers', 'following',
+                     'type']
+non_persitent_keys = ['contributions']
 
 
 def get_user(document, update=True):
     document = parse(document)
-    document = trim_json(document, user_keys_to_keep)
 
     connection = MongoConnection()
     collection = connection.get_collection('github_users')
     user = collection.find_one({'id': document['id']})
     if user is None:
-        user = {}
-        for key in document:
-            if key in user_keys_to_keep and key not in non_persitant_keys:
-                user[key] = document[key]
+        user = trim_json(document, persistent_keys)
         user['type'] = get_type_id(document['type'])
         JSONEncoder().encode(user)
         collection.insert(user, check_keys=False)
         user = collection.find_one({'id': document['id']})
     elif update:
         for key in document:
-            user[key] = document[key]
+            if key in persistent_keys:
+                user[key] = document[key]
         user['type'] = get_type_id(document['type'])
         collection.update_one({'_id': user['_id']}, {"$set": user}, upsert=True)
-    for key in non_persitant_keys:
-        if key not in user and key in document:
+    for key in non_persitent_keys:
+        if key in document:
             user[key] = document[key]
     return user
 
